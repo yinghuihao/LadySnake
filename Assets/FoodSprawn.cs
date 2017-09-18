36977ad29c8c54bfa6c8bfba4a4cc84dc3763b70
郝迎晖 <haoyinghui@kakumukahes-MacBook-Pro.local>
2017-09-18 11:57:54 -0700

    Week 3 version

---
+++ Assets/FoodSprawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSprawn : MonoBehaviour {
+	//Borders
+	public Transform border_top;
+	public Transform border_bottom;
+	public Transform border_left;
+	public Transform border_right;
+
+	//Food Prefab
+	public GameObject food;
+	//Exit Prefab
+	public GameObject exit;
+
+	//Spawn a piece of food randomly within borders
+	void randomSpawn(){
+		int x = (int)Random.Range (border_left.position.x, border_right.position.x);
+		int y = (int)Random.Range (border_bottom.position.y, border_top.position.y);
+		Instantiate (food, new Vector2(x, y), Quaternion.identity);
+	}
+
+	void randomExit(){
+		int x = (int)Random.Range (border_left.position.x, border_right.position.x);
+		int y = (int)Random.Range (border_bottom.position.y, border_top.position.y);
+		Instantiate (exit, new Vector2(x, y), Quaternion.identity);
+	}
+		
+	// Use this for initialization
+	void Start () {
+		exit = Resources.Load("exitPrefab") as GameObject;
+		Debug.Log ("exit: " + exit);
+		Invoke ("randomExit", 20);
+		InvokeRepeating ("randomSpawn", 3, 4);
+	}
+}
---
+++ Assets/Snake.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+
+public class Snake : MonoBehaviour {
+	//By default the snake moves to right
+	public float currentRotation;
+	public float tailRotation;
+	public List<Transform> tail = new List<Transform> ();
+	public GameObject t;
+	bool ate = false;
+	bool rotated = false;
+	public GameObject TailPrefab;
+	public GameObject tPrefab;
+	public Vector2 curdir = Vector2.right;
+	public Vector2 taildir = Vector2.left;
+
+//	private int level = 1;
+	private bool exit = false;
+	// Use this for initialization
+	void Start () {
+		t = (GameObject)Instantiate (tPrefab, new Vector2(transform.position.x-1, transform.position.y), Quaternion.identity);
+		InvokeRepeating ("Move", 0.3f, 0.3f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (exit) {
+			return;
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			if (curdir == Vector2.up) {
+				currentRotation += -90.0f;
+				curdir = Vector2.right;
+			} else if (curdir == -Vector2.up) {
+				currentRotation += 90.0f;
+				curdir = Vector2.right;
+			} else {
+				currentRotation += 0.0f;
+			}
+			rotated = true;
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			if (curdir == -Vector2.right) {
+				currentRotation += 90.0f;
+				curdir = -Vector2.up;
+			} else if (curdir == Vector2.right) {
+				currentRotation += -90.0f;
+				curdir = -Vector2.up;
+			}else {
+				currentRotation += 0.0f;
+			}
+			rotated = true;
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			if (curdir == Vector2.up) {
+				currentRotation += 90.0f;
+				curdir = -Vector2.right;
+			} else if (curdir == -Vector2.up) {
+				currentRotation += -90.0f;
+				curdir = -Vector2.right;
+			}else {
+				currentRotation += 0.0f;
+			}
+			rotated = true;
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
+			if (curdir == -Vector2.right) {
+				currentRotation += -90.0f;
+				curdir = Vector2.up;
+			} else if (curdir == Vector2.right) {
+				currentRotation += 90.0f;
+				curdir = Vector2.up;
+			}else {
+				currentRotation += 0.0f;
+			}
+			rotated = true;
+		}
+	}
+
+	void calculateDir(Vector2 lastpos, Vector2 curpos){
+		if (curpos.x - lastpos.x == 1) {//Heading Right
+			if (taildir == Vector2.up) {
+				tailRotation += 90.0f;
+			} else if (taildir == Vector2.down) {
+				tailRotation += -90.0f;
+			} else {
+				tailRotation += 0.0f;
+			}
+			taildir = Vector2.left;
+		} else if (curpos.x - lastpos.x == -1) {//Heading Left
+			if (taildir == Vector2.up) {
+				tailRotation += -90.0f;
+			} else if (taildir == Vector2.down) {
+				tailRotation += 90.0f;
+			} else {
+				tailRotation += 0.0f;
+			}
+			taildir = Vector2.right;
+		} else if (curpos.y - lastpos.y == 1) {//Heading Up
+			if (taildir == Vector2.right) {
+				tailRotation += -90.0f;
+			} else if (taildir == Vector2.left) {
+				tailRotation += 90.0f;
+			} else {
+				tailRotation += 0.0f;
+			}
+			taildir = Vector2.down;
+		} else if (curpos.y - lastpos.y == -1) {
+			if (taildir == Vector2.right) {
+				tailRotation += 90.0f;
+			} else if (taildir == Vector2.left) {
+				tailRotation += -90.0f;
+			} else {
+				tailRotation += 0.0f;
+			}
+			taildir = Vector2.up;
+		}
+	}
+
+	void Move(){
+		if (exit) {
+			return;
+		}
+		Vector2 v = transform.position;
+		if (ate) {
+			GameObject g = (GameObject)Instantiate (TailPrefab, v, Quaternion.identity);
+			tail.Insert (0, g.transform);
+			//if (tail.Count == 1) {
+				//tailRotation = 0.0f;
+			//}
+			ate = false;
+		} else if (tail.Count > 0) {
+			Vector2 lastpos = tail.Last ().position;
+			tail.Last ().position = v;
+			tail.Insert (0, tail.Last ());
+			tail.RemoveAt (tail.Count - 1);
+			t.transform.position = lastpos;
+			Vector2 curpos = tail.Last ().position;
+			calculateDir (lastpos, curpos);
+			t.transform.rotation = Quaternion.Euler(new Vector3(tPrefab.transform.rotation.x, tPrefab.transform.rotation.y, tailRotation));
+		} else if (tail.Count == 0) {
+			t.transform.position = v;
+		}
+
+		if (curdir == Vector2.right) {
+			transform.position = new Vector3 (transform.position.x + 1, transform.position.y, transform.position.z);
+		} else if (curdir == Vector2.up) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z);
+		} else if (curdir == -Vector2.right) {
+			transform.position = new Vector3 (transform.position.x - 1, transform.position.y, transform.position.z);
+		} else if (curdir == -Vector2.up) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y - 1, transform.position.z);
+		}
+
+		if (rotated == true) {
+			transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y, currentRotation));
+			if (tail.Count == 0) {
+				//tailRotation += currentRotation;
+				taildir = curdir;
+				t.transform.rotation = Quaternion.Euler(new Vector3(tPrefab.transform.rotation.x, tPrefab.transform.rotation.y, currentRotation));
+				tailRotation = currentRotation;
+			}
+			rotated = false;
+		}
+	}
+
+//	void SnakeDisappear(){
+//		while (tail.Count > 0) {
+//			Debug.Log ("tailCount" + tail.Count);
+//			removeBody ();
+//		}
+//		if (tail.Count == 0) {
+//			Destroy (tPrefab);
+//		}
+//
+//	}
+//
+//	void removeBody(){
+//		Debug.Log ("removeCalled" + tail);
+//		tail.RemoveAt (0);
+//	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		if (coll.name.StartsWith ("FoodPrefab")) {
+			ate = true;
+			Destroy (coll.gameObject);
+		}else if (coll.name.StartsWith ("exit")) {
+			exit = true;
+//			Debug.Log ("tail" + tail.Count);
+			while (tail.Count > 0) {
+				tail.RemoveAt (tail.Count - 1);
+//				Invoke ("removeBody", 0.3f);
+				Debug.Log ("tail" + tail.Count);
+			}
+			EditorUtility.DisplayDialog ("Congrats", "Going to next level", "OK");
+			SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+		} else {
+			EditorUtility.DisplayDialog ("Oops", "Don't hit the wall", "OK");
+			SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+		}
+	}		
+}
